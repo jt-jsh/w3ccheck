1982cb12c4e8f4f44304eaab93471386dd69c6b3
jews3333 <jews333333@gmail.com>
2020-06-02 13:53:31 +0900

    파이어베이스 연동

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC_9nl8hJS610wNCUfFYNYMzcifzR2fQQg",
+    authDomain: "w3c-check-5db0b.firebaseapp.com",
+    databaseURL: "https://w3c-check-5db0b.firebaseio.com",
+    projectId: "w3c-check-5db0b",
+    storageBucket: "w3c-check-5db0b.appspot.com",
+    messagingSenderId: "917024716815",
+    appId: "1:917024716815:web:ef2f6103920bd225c68b7a"
+}
---
+++ src/firebase/init.js
@@ -0,0 +1,4 @@
+import firebase from 'firebase';
+import Config from './config';
+
+firebase.initializeApp(Config);
